fb3145dbba63a52d82911251420b0071bffe5093
Dylan Lott <lott.dylan@gmail.com>
2016-03-02 23:35:47 -0700

    added more routes

---
+++ routes/LocationRoutes.js
@@ -0,0 +1,22 @@
+module.exports = function(){
+  var express = require('express');
+  var app = express();
+
+  app.get('/', function(req, res){
+
+  });
+  app.get('/:id', function(req, res){
+  	
+  });
+  app.post('/', function(req, res){
+  	
+  });
+  app.put('/:id', function(req, res){
+  	
+  });
+  app.delete('/:id', function(req, res){
+  	
+  });
+
+  return app;
+}();
